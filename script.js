5858ff0d94a2de9de1f3520e5e627d674a09f4e1
vaibhav-60 <vaibhavrai974@gmail.com>
2024-07-17 15:34:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function showContent(id) {
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => {
+        content.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+}
